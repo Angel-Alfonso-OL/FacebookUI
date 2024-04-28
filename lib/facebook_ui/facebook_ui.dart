f690773e620ead40570a0d1ce80db8edda0c7793
Angel Orozco <angelalfonsool20@gmail.com>
2024-04-27 17:57:35 -0700

    Images, Icons added

---
+++ lib/facebook_ui/facebook_ui.dart
@@ -0,0 +1,20 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class FacebookUI extends StatelessWidget {
+  const FacebookUI({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        systemOverlayStyle: SystemUiOverlayStyle.dark,
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: ,
+      ),
+    );
+  }
+}
